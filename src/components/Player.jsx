f4631599801eeabc2b9d1e99007fda4a5ebe745f
Attmoon <99157555+Attmoon@users.noreply.github.com>
2024-07-31 08:08:53 +0900

    조건적 콘텐츠& state 업데이트

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,19 +4,23 @@ export default function Player({name, symbol}) {
     const [ isEditing, setIsEditing ] = useState(false);
 
     function handleEditClick() {
-        setIsEditing(true);
+        setIsEditing(!isEditing);
     }
+    
     let palyerName = <span className="player-name">{name}</span>;
+    // let btnCaption = 'Edit';
     if (isEditing) {
-        palyerName = <input type="text" required />;
+        palyerName = <input type="text" required value={name}/>;
+        // btnCaption = 'Save';
     }
+
     return (
         <li>
             <span className="player">
                 {palyerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={handleEditClick}>Edit</button>
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
         </li>
     );
 }
