08ee16fab3135ad6094d46b6ab429646cbff6f8f
Attmoon <99157555+Attmoon@users.noreply.github.com>
2024-08-01 08:35:12 +0900

    다차원 리스트 렌더링

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
+
 function App() {
   return <main>
     <div id="game-container">
@@ -6,7 +8,7 @@ function App() {
         <Player initialName="Player1" symbol="X" />
         <Player initialName="Player2" symbol="O" />
       </ol>
-       GAME BOARD
+       <GameBoard />
       
     </div>
 
---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,19 @@
+const initialGanmeBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+];
+
+export default function GameBoard() {
+    return <ol id="game-board">
+        {initialGanmeBoard.map((row, rowIndex) => <li key={rowIndex}>
+            <ol>
+                {row.map((playerSymbol, colIndex) => (
+                <li key={colIndex}>
+                    <button>{playerSymbol}</button>
+                </li>
+            ))}
+            </ol>
+        </li>)}
+    </ol>
+}
