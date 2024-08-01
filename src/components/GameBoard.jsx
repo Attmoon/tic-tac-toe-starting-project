c9d7e55c6cc6fef1a7ed18a4cfa10dcf9bcc29a8
Attmoon <99157555+Attmoon@users.noreply.github.com>
2024-08-02 08:35:06 +0900

    lifting state up

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,15 +6,17 @@ const initialGanmeBoard = [
     [null, null, null]
 ];
 
-export default function GameBoard() {
+export default function GameBoard( {onSelectSquare, activePlayerSymbol} ) {
     const [gameBoard, setGameBoard] =  useState(initialGanmeBoard);
 
     function hadnleSelectSquare(rowIndex, colIndex) {
         setGameBoard((prevGameBoard) => {
             const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-            updatedBoard[rowIndex][colIndex] = 'X';
+            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
             return updatedBoard;
         });
+
+        onSelectSquare();
     }
 
     return <ol id="game-board">
